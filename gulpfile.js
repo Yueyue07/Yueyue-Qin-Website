5eaed7c99f2c43dbe708ab1df2078d7dc52e4433
Yueyue07 <xww.qyy@gmail.com>
2016-04-28 13:00:54 -0700

    add gulp for piping file into build folder

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const webpack = require('webpack-stream');
+const babel = require('babel-loader');
+const html = require('html-loader');
+const sass = require('gulp-sass');
+const maps = require('gulp-sourcemaps');
+
+gulp.task('html:dev', () => {
+  gulp.src(__dirname + '/app/**/*.html')
+    .pipe(gulp.dest(__dirname + '/build'));
+});
+
+gulp.task('sass:dev', () => {
+  gulp.src(__dirname + '/app/**/*.scss')
+    .pipe(maps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(__dirname + '/build'));
+});
+
+
+gulp.task('venderjs:dev', () => {
+  gulp.src(__dirname + '/app/js/vender/*.js')
+    .pipe(gulp.dest(__dirname + '/build/vender'));
+});
+
+gulp.task('webpack:dev', () => {
+  gulp.src(__dirname + '/app/js/client.js')
+    .pipe(webpack({
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+      .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build:dev', ['html:dev', 'sass:dev',
+  'venderjs:dev', 'webpack:dev']);
