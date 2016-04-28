8a763737eae2684345b93464672dd2fe649cb40c
Yueyue07 <xww.qyy@gmail.com>
2016-04-28 14:38:48 -0700

    change mongo environmental variables

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGOLAB_URI
+mongoose.connect(process.env.MONGODB_URI
   || 'mongodb://localhost/images_app_dev');
 
 
